7f0d1ec94199e5ab550e8094496a499a9e76fc92
danCode-s <dancodes9@gmail.com>
2024-09-07 12:09:48 +0500

    time complexity

--- recursionPt2.java
+++ recursionPt2.java
@@ -3,6 +3,9 @@ public class recursionPt2 {
     public static void towerOfHanoi(int n, String source, String helper, String dest){
         // 1 disk is the top disk.
 
+        // time complexity --> O(2^n - 1) =~ O(2^n),
+        // recursion equation T(n) = 2T(n-1)+1
+
         if (n==1){
             System.out.println("Transfer disk "+ n + " from "+ source+" to "+dest);
             return;
